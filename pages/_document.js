c6f6d84d240074dd1b59fc8e284f43f5bf0f78b3
bett <bettkipronoh80@gmail.com>
2025-07-03 22:42:17 +0300

    your message

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        {/* Force light mode and consistent browser theme */}
+        <meta name="color-scheme" content="light only" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
