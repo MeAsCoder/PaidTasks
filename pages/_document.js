951d75ebd7f626a9e078a63050be0c5f606ddd46
bett <bettkipronoh80@gmail.com>
2026-04-02 20:54:47 +0300

    Added Favicon to the website

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
         {/* Force light mode and consistent browser theme */}
         <meta name="color-scheme" content="light only" />
         <meta name="theme-color" content="#ffffff" />
+        
+         <link rel="icon" type="image/png" href="/favcon.png" />
       </Head>
       <body className="antialiased">
         <Main />
